b13726b785778c09aecac276f934834607c0b90f
mom-pc <mom-pc@fake.com>
2020-02-12 16:51:39 +0300

    Добавлчет фаил лля конфигурации тестов

--- src/components/main/main.e2e.test.js
+++ src/components/main/main.e2e.test.js
@@ -1,11 +1,6 @@
 import React from "react";
-import Enzyme, {shallow} from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import {shallow} from "enzyme";
 import Main from "./main.jsx";
-
-Enzyme.configure({
-  adapter: new Adapter(),
-});
 
 const mockData = {
   filmOnHeader: {
