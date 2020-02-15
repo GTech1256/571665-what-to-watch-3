2aa775e19baf4f82cc0321fa11a32cf09cbbcbfa
GTech1256 <pomah156@gmail.com>
2020-02-15 17:59:44 +0300

    Добавляет функция-обработчик наведения компоненту карточки фильма и ее тип для валидации

--- src/components/film-card/film-card.jsx
+++ src/components/film-card/film-card.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const FilmCard = ({name}) => (
-  <article className="small-movie-card catalog__movies-card">
+const FilmCard = ({
+  name,
+  onHover
+}) => (
+  <article
+    className="small-movie-card catalog__movies-card"
+    onMouseOver={onHover}
+  >
     <div className="small-movie-card__image">
       <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt={name} width="280" height="175" />
     </div>
@@ -13,7 +19,8 @@ const FilmCard = ({name}) => (
 );
 
 FilmCard.propTypes = {
-  name: PropTypes.string.isRequired
+  name: PropTypes.string.isRequired,
+  onHover: PropTypes.func.isRequired
 };
 
 export default FilmCard;
