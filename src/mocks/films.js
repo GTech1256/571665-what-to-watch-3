5a2f19ea305404cad3376a173ece8507692ed154
GTech1256 <pomah156@gmail.com>
2020-02-15 17:50:13 +0300

    Добавляет файл мока для фильма из 8 элементов

---
+++ src/mocks/films.js
@@ -0,0 +1,10 @@
+export default [
+  `Fantastic Beasts`,
+  `Bohemian Rhapsody`,
+  `Macbeth`,
+  `The Godfather`,
+  `The Wizard of Oz`,
+  `Citizen Kane`,
+  `The Shawshank Redemption`,
+  `Pulp Fiction`
+];
