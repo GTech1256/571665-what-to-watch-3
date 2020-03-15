b1ffdecd1747c8f0f65fce272896d3e80a5a1fc8
Roman Bakakin <pomah156@gmail.com>
2020-03-15 16:32:30 +0300

    Удалит е2е тест для Видео Плеера

--- src/components/video-player/video-player.test.js
+++ src/components/video-player/video-player.test.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import renderer from "react-test-renderer"
-import VideoPlayer from './video-player'
+import React from "react";
+import renderer from "react-test-renderer";
+import VideoPlayer from "./video-player";
 
 const mock = {
   isPlaying: false,
